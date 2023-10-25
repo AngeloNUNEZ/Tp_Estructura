242b9be3ab0fec4bb01c60c14bc4a547ca26680b
AngeloNUNEZ <angeloarielnunezenciso@fpuna.edu.py>
2023-10-25 10:57:56 -0300

    se agrega c#

---
+++ C#/Procesamiento/Program.cs
@@ -0,0 +1,254 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using WordCloudCsharp;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ScottPlot;
+
+
+class Articulos
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public string Revista { get; set; }
+    public string Año { get; set; }
+    public string Editor { get; set; }
+    public string URL { get; set; }
+    public string Citado { get; set; }
+
+    public Articulos()
+    {
+        Titulo = string.Empty;
+        Autor = string.Empty;
+        Revista = string.Empty;
+        Año = string.Empty;
+        Editor = string.Empty;
+        URL = string.Empty;
+        Citado = string.Empty;
+    }
+}
+class Articulo
+{
+    public string Citado { get; set; }
+    public string Año { get; set; }
+    public string Titulo { get; set; }
+    public string Revista { get; set; }
+
+    public Articulo()
+    {
+        Citado = string.Empty;
+        Año = string.Empty;
+        Titulo = string.Empty;
+        Revista = string.Empty;
+    }
+}
+class Articulo2
+{
+    public string Titulo { get; set; }
+    public string URL { get; set; }
+
+    public Articulo2()
+    {
+        Titulo = string.Empty;
+        URL = string.Empty;
+    }
+}
+class Autores
+{
+    public string Autor { get; set; }
+    public string Titulo { get; set; }
+
+    public Autores()
+    {
+        Autor = string.Empty;
+        Titulo = string.Empty;
+    }
+}
+
+class Program
+{
+
+
+    static void Main()
+    {
+        // Cargar el DataFrame desde el archivo CSV
+        using (var reader = new StreamReader("web_scraping_citas.csv"))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            var records = csv.GetRecords<Articulos>().ToList();
+
+            /*************************1******************************/
+            // Ordenar el DataFrame por número de citas en orden descendente
+             var dfOrdenado = records.OrderByDescending(record => double.Parse(record.Citado, CultureInfo.InvariantCulture)).ToList();
+
+            // Seleccionar los 10 artículos con más citas
+            var top10Citados = dfOrdenado.Take(10).ToList();
+
+
+
+            // Crear una lista de objetos de la clase Articulo
+            var resultado = top10Citados.Select(record => new Articulo
+            {
+                Citado = record.Citado,
+                Año = record.Año,
+                Titulo = record.Titulo,
+                Revista = record.Revista
+            }).ToList();
+
+
+            // Guardar el resultado en un archivo CSV
+            using (var writer = new StreamWriter("top_10_articulos_citados.csv"))
+            using (var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csvWriter.WriteRecords(resultado);
+            }
+
+            /*************************2******************************/
+            // Solicitar al usuario que ingrese la palabra clave de interés
+            Console.Write("Ingrese la palabra clave de interés: ");
+            string palabraClave = Console.ReadLine();
+
+            // Filtrar los artículos que contienen la palabra clave en el título
+            var resultados = records.Where(record => record.Titulo.Contains(palabraClave, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            // Crear un DataFrame con los títulos y URLs de los artículos que coinciden
+            var resultadosFiltrados = resultados.Select(record => new Articulo2
+            {
+                Titulo = record.Titulo,
+                URL = record.URL
+            }).ToList();
+
+            // Guardar el resultado en un archivo CSV
+            using (var writer = new StreamWriter("resultados_palabra_clave.csv"))
+            using (var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csvWriter.WriteRecords(resultadosFiltrados);
+            }
+
+            Console.WriteLine("Se han guardado los resultados en 'resultados_palabra_clave.csv'.");
+
+            /*************************3******************************/
+            // Extraer la lista de autores
+            var autores = records
+                .Where(record => !string.IsNullOrEmpty(record.Autor))
+                .Select(record => record.Autor)
+                .ToList();
+
+            // Crear una lista plana de autores
+            var listaAutores = autores
+                .SelectMany((string autor) => autor?.Split(new string[] { " and " }, StringSplitOptions.None) ?? Enumerable.Empty<string>())
+                .ToList();
+
+            // Contar la frecuencia de cada autor
+            var frecuenciaAutores = listaAutores
+                .GroupBy(autor => autor)
+                .Select(group => new
+                {
+                    Autor = group.Key,
+                    Frecuencia = group.Count()
+                })
+                .ToList();
+
+            // Ordenar la lista de autores por el número de veces que aparecen
+            var frecuenciaAutoresOrdenada = frecuenciaAutores
+                .OrderByDescending(autor => autor.Frecuencia)
+                .ToList();
+
+            // Guardar el resultado en un archivo CSV
+            using (var writer = new StreamWriter("autores_frecuencia.csv"))
+            using (var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csvWriter.WriteRecords(frecuenciaAutoresOrdenada);
+            }
+
+            Console.WriteLine("Se ha guardado la frecuencia de los autores en 'autores_frecuencia.csv'.");
+
+            /*************************4******************************/
+            // Obtener los títulos del DataFrame
+            var titulos = records.Select(record => record.Titulo).ToList();
+
+            // Crear una lista de palabras significativas para omitir
+            List<string> palabrasOmitir = new List<string>
+            {
+                "a", "an", "the", "in", "of", "for", "and", "on", "with", "to", "by", "at"
+            };
+
+            // Procesar los títulos y contar las palabras
+            List<string> todasPalabras = new List<string>();
+            foreach (var titulo in titulos)
+            {
+                var palabras = titulo.ToLower().Split(' ');
+                foreach (var palabra in palabras)
+                {
+                    if (!palabrasOmitir.Contains(palabra))
+                    {
+                        todasPalabras.Add(palabra);
+                    }
+                }
+            }
+
+            // Crear un diccionario de frecuencias de palabras
+            Dictionary<string, int> contadorPalabras = new Dictionary<string, int>();
+            foreach (var palabra in todasPalabras)
+            {
+                if (contadorPalabras.ContainsKey(palabra))
+                {
+                    contadorPalabras[palabra]++;
+                }
+                else
+                {
+                    contadorPalabras[palabra] = 1;
+                }
+            }
+
+            // Configurar el tamaño de la imagen de la nube de palabras
+            var width = 800;
+            var height = 400;
+
+            // Generar la nube de palabras a partir del diccionario de frecuencias
+            var wordcloud = ISingletion<WordcloudSrv>.Instance
+                .GetWordCloud(width, height)
+                .Draw(contadorPalabras.Keys.ToList(), contadorPalabras.Values.ToList());
+
+            // Guardar la imagen en el disco
+            wordcloud.Save("wordcloud.png", System.Drawing.Imaging.ImageFormat.Png);
+
+            Console.WriteLine("Nube de palabras generada y guardada en 'wordcloud.png'.");
+
+            // Obtener la cantidad de artículos por año
+            var conteoAnios = records
+                .GroupBy(record => record.Año)
+                .Select(group => new { Anio = group.Key, Cantidad = group.Count() })
+                .OrderBy(result => result.Anio)
+                .ToList();
+
+            // Crear un arreglo de años, cantidades y posiciones personalizadas
+            var cantidades = conteoAnios.Select(result => (double)result.Cantidad).ToArray();
+            var posiciones = conteoAnios.Select((result, index) => (double)index).ToArray();  // Posiciones personalizadas
+
+            // Crear un objeto Plot
+            var plt = new ScottPlot.Plot(600, 400);
+
+            // Agregar un gráfico de barras al plot con posiciones personalizadas
+            plt.AddBar(cantidades, posiciones);
+
+            // Ajustar los límites del eje para eliminar el espacio por debajo del gráfico de barras
+            plt.SetAxisLimits(yMin: 0);
+
+            // Guardar la imagen en el disco
+            plt.SaveFig("bar_graph.png");
+            Console.WriteLine("Grafico de barras generado y guardado en 'bar_graph.png'.");
+
+        }
+
+    }
+
+
+
+}
---
+++ C#/Scrape/Program.cs
@@ -0,0 +1,226 @@
+﻿using System.Data;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Text;
+using System.Text.RegularExpressions;
+using BibTeXLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        // Crear un DataTable con los datos
+        DataTable dataTable = new DataTable();
+        dataTable.Columns.Add("Titulo");
+        dataTable.Columns.Add("Autor");
+        dataTable.Columns.Add("Revista");
+        dataTable.Columns.Add("Año");
+        dataTable.Columns.Add("Editor");
+        dataTable.Columns.Add("URL");
+        dataTable.Columns.Add("Citado");
+
+        var chromeOptions = new ChromeOptions();
+        chromeOptions.AddArguments("--start-maximized");
+        IWebDriver driver = new ChromeDriver(chromeOptions);
+        driver.Navigate().GoToUrl("https://scholar.google.com/scholar?cites=5866269323493626547&as_sdt=2005&sciodt=0,5&hl=es");
+
+        // Esperar hasta que los resultados de la búsqueda se carguen
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(300));
+        wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.Id("gs_res_ccl")));
+
+        // Extraer todos los artículos de una página
+        IList<IWebElement> articulos = driver.FindElements(By.XPath("//*[@id='gs_res_ccl']//h3"));
+        int secondsPause = 1;
+        
+
+        for (int i = 0; i < articulos.Count; i++)
+        {
+            /**Recargar los datos de la pagina**/
+            // Esperar hasta que los resultados de la búsqueda se carguen
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(300));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.Id("gs_res_ccl")));
+
+            // Extraer todos los artículos de una página
+            articulos = driver.FindElements(By.XPath("//*[@id='gs_res_ccl']//h3"));
+            /**********************************/
+            Console.WriteLine(articulos[i].Text);
+            int divIndex = i + 1;
+            string link = "";
+
+            try
+            {
+                link = new WebDriverWait(driver, TimeSpan.FromSeconds(20)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath($@"//*[@id='gs_res_ccl_mid']/div[{divIndex}]/div[2]/div[3]/a[3]"))).GetAttribute("href");
+                Console.WriteLine(link);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error al encontrar el elemento: " + e);
+            }
+
+            string xpath = $@"//*[@id='gs_res_ccl_mid']/div[{divIndex}]/div[2]/div[3]/a[3]";
+            string citadoPor = "No disponible";
+
+            try
+            {
+                citadoPor = articulos[i].FindElement(By.XPath(xpath)).Text;
+            }
+            catch (NoSuchElementException)
+            {
+                citadoPor = "No disponible";
+            }
+
+            int numCitas = 0;
+
+            // Utiliza una expresión regular para extraer el número de citas
+            Match match = Regex.Match(citadoPor, @"\d+");
+            if (match.Success)
+            {
+                numCitas = int.Parse(match.Value);
+            }
+
+            Console.WriteLine("Citado por: " + numCitas);
+
+            try
+            {
+                IWebElement enlaceCitar = articulos[i].FindElement(By.XPath($@"//*[@id='gs_res_ccl_mid']/div[{divIndex}]/div[2]/div[3]/a[2]"));
+                Console.WriteLine("Enlace Citar: " + enlaceCitar.GetAttribute("href"));
+                secondsPause = new Random().Next(10, 15);
+                Thread.Sleep(TimeSpan.FromSeconds(secondsPause));
+                enlaceCitar.Click();
+
+                try
+                {
+                    secondsPause = new Random().Next(10, 15);
+                    Thread.Sleep(TimeSpan.FromSeconds(secondsPause));
+                    IWebElement enlaceBibTeX = new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//*[@id='gs_citi']/a[1]")));
+                    Console.WriteLine("Enlace BibTeX: " + enlaceBibTeX.GetAttribute("href"));
+                    enlaceBibTeX.Click();
+                    secondsPause = new Random().Next(10, 15);
+                    Thread.Sleep(TimeSpan.FromSeconds(secondsPause));
+                    Console.WriteLine("Click en bibtex");
+                    
+                    try
+                    {
+                        // Esperar a que ciertos elementos se carguen completamente
+                        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                        wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body/pre")));
+                        IWebElement bibtexElement = driver.FindElement(By.XPath("/html/body/pre"));
+                        string bibtexData = bibtexElement.Text;
+                        Console.WriteLine("bibtex_data: " + bibtexData);
+                        // Crear un analizador BibParser a partir de la cadena de texto
+                        var parser = new BibParser(new StringReader(bibtexData));
+                        Console.WriteLine("Logro crear parser: "+parser);
+                        // Obtener todas las entradas BibTeX (puede haber varias)
+                        var entries = parser.GetAllResult();
+                        Console.WriteLine("Logro crear entries: "+entries);
+                        secondsPause = new Random().Next(15, 20);
+                        Thread.Sleep(TimeSpan.FromSeconds(secondsPause));
+                        if (entries.Count > 0)
+                        {
+                            Console.WriteLine("Entro al if ");
+                            // Tomar la primera entrada (parsed_entries[0])
+                            var entry = entries[0];
+
+                            // Acceder a los campos BibTeX directamente
+                            string title = entry.Title;
+                            string author = entry["author"];
+                            string journal = entry["journal"];
+                            string year = entry["year"];
+                            string publisher = entry["publisher"];
+
+                            // Crear una nueva fila en el DataTable
+                            DataRow newDataRow = dataTable.NewRow();
+                            newDataRow["Titulo"] = title;
+                            newDataRow["Autor"] = author;
+                            newDataRow["Revista"] = journal;
+                            newDataRow["Año"] = year;
+                            newDataRow["Editor"] = publisher;
+                            newDataRow["URL"] = link;
+                            newDataRow["Citado"] = numCitas;
+
+                            // Agregar la nueva fila al DataTable
+                            dataTable.Rows.Add(newDataRow);
+
+                            // Visualizar los datos
+                            Console.WriteLine("Datos BibTeX:");
+                            Console.WriteLine("Título: " + title);
+                            Console.WriteLine("Autor: " + author);
+                            Console.WriteLine("Revista: " + journal);
+                            Console.WriteLine("Año: " + year);
+                            Console.WriteLine("Editor: " + publisher);
+                            Console.WriteLine("Salio del if ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Entro al else ");
+                            // Manejar el caso en que no se encuentren datos BibTeX válidos
+                            Console.WriteLine("No se encontraron datos BibTeX válidos para el artículo: " + articulos[i].Text);
+                            Console.WriteLine("Salio del else ");
+                        }
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error al procesar los datos BibTeX para el artículo: " + articulos[i].Text);
+                        Console.WriteLine(ex);
+                    }
+                    // Volver atrás para el siguiente artículo
+                    secondsPause = new Random().Next(15, 20);
+                    Thread.Sleep(TimeSpan.FromSeconds(secondsPause));
+                    driver.Navigate().GoToUrl("https://scholar.google.com/scholar?cites=5866269323493626547&as_sdt=2005&sciodt=0,5&hl=es");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("No se encontró el enlace 'BibTeX' para el artículo: ");
+                    Console.WriteLine(ex);
+                }
+                Console.WriteLine("Salio del try bibtex parseo ");
+                // Volver atrás
+                secondsPause = new Random().Next(20, 35);
+                Thread.Sleep(TimeSpan.FromSeconds(secondsPause));
+                driver.Navigate().GoToUrl("https://scholar.google.com/scholar?cites=5866269323493626547&as_sdt=2005&sciodt=0,5&hl=es");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se encontró el enlace 'Citar' para el artículo: ");
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("Salio del try citar ");
+
+        }//for
+        // Almacenar el DataTable en un archivo CSV
+        DataTableToCsv(dataTable, "web_scraping_citas.csv");
+
+        Thread.Sleep(TimeSpan.FromSeconds(60));
+
+        // Cerrar el navegador
+        driver.Quit();
+
+        // Mostrar los primeros registros del DataTable
+        Console.WriteLine("Datos obtenidos:");
+        for (int i = 0; i < Math.Min(dataTable.Rows.Count, 5); i++)
+        {
+            DataRow row = dataTable.Rows[i];
+            Console.WriteLine($"Título: {row["Titulo"]}, Autor: {row["Autor"]}, Revista: {row["Revista"]}, Año: {row["Año"]}, Editor: {row["Editor"]}, URL: {row["URL"]}, Citado: {row["Citado"]}");
+        }
+
+    }//main
+
+    // Función para guardar el DataTable en un archivo CSV
+    private static void DataTableToCsv(DataTable dataTable, string filePath)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        IEnumerable<string> columnNames = dataTable.Columns.Cast<DataColumn>().Select(column => column.ColumnName);
+        sb.AppendLine(string.Join(",", columnNames));
+
+        foreach (DataRow row in dataTable.Rows)
+        {
+            IEnumerable<string> fields = row.ItemArray.Select(field => field.ToString());
+            sb.AppendLine(string.Join(",", fields));
+        }
+
+        File.WriteAllText(filePath, sb.ToString());
+    }
+}
